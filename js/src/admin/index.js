d6ef603256c09282f7429f4204eabfe595bfae4b
Ian Morland <ian@morland.me>
2020-12-13 14:51:09 +0000

    Lint JS

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -8,15 +8,15 @@ const networks = [
 ];
 
 app.initializers.add('fof/share-social', () => {
+    let set = app.extensionData.for('fof-share-social');
 
-  let set = app.extensionData.for('fof-share-social');
-
-  networks.map(networks =>
-    networks.map(network => (
-      set.registerSetting({
-        label: app.translator.trans(`fof-share-social.lib.networks.${network}`),
-        setting: `fof-share-social.networks.${network}`,
-        type: 'boolean'
-      })
-    )))
+    networks.map(networks =>
+        networks.map(network =>
+            set.registerSetting({
+                label: app.translator.trans(`fof-share-social.lib.networks.${network}`),
+                setting: `fof-share-social.networks.${network}`,
+                type: 'boolean',
+            })
+        )
+    );
 });
