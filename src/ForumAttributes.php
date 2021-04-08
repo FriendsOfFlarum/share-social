e5a2d8a86ef905b8af262d62f0cee06857b2312e
Clark Winkelmann <clarkwinkelmann@users.noreply.github.com>
2021-04-08 23:42:06 +0000

    Apply fixes from StyleCI

--- src/ForumAttributes.php
+++ src/ForumAttributes.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of fof/share-social.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\ShareSocial;
 
 use Flarum\Api\Serializer\ForumSerializer;
@@ -23,7 +32,7 @@ class ForumAttributes
         $networks = [];
 
         foreach (self::KEYS as $key) {
-            if ($this->settings->get(self::PREFIX . $key)) {
+            if ($this->settings->get(self::PREFIX.$key)) {
                 $networks[] = $key;
             }
         }
