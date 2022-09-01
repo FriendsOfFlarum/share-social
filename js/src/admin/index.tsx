b5c550a8eea6f01d5a3ebc71fefb8599d85e8dd2
Ngo Quoc Dat <datlechin@gmail.com>
2022-09-01 22:11:23 +0700

    reformat admin code

--- js/src/admin/index.tsx
+++ js/src/admin/index.tsx
@@ -1,33 +1,38 @@
 import app from 'flarum/admin/app';
 
 const networks = [
-  ['facebook', 'twitter', 'linkedin', 'reddit'],
-  ['whatsapp', 'telegram'],
-  ['vkontakte', 'odnoklassniki', 'my_mail'],
-  ['qq', 'qzone'],
-  ['native'],
+  'facebook',
+  'twitter',
+  'linkedin',
+  'reddit',
+  'whatsapp',
+  'telegram',
+  'vkontakte',
+  'odnoklassniki',
+  'my_mail',
+  'qq',
+  'qzone',
+  'native',
 ];
 
 app.initializers.add('fof/share-social', () => {
-  let set = app.extensionData.for('fof-share-social');
+  const set = app.extensionData.for('fof-share-social');
 
-  set.registerSetting({
-    label: app.translator.trans('fof-share-social.admin.settings.canonical-urls'),
-    setting: 'fof-share-social.canonical-urls',
-    type: 'boolean',
-  });
+  set
+    .registerSetting({
+      label: app.translator.trans('fof-share-social.admin.settings.canonical-urls'),
+      setting: 'fof-share-social.canonical-urls',
+      type: 'boolean',
+    })
+    .registerSetting(function () {
+      return <hr />;
+    });
 
-  set.registerSetting(function () {
-    return <hr />;
-  });
-
-  networks.forEach((networks) =>
-    networks.forEach((network) =>
-      set.registerSetting({
-        label: app.translator.trans(`fof-share-social.lib.networks.${network}`),
-        setting: `fof-share-social.networks.${network}`,
-        type: 'boolean',
-      })
-    )
+  networks.forEach((network) =>
+    set.registerSetting({
+      label: app.translator.trans(`fof-share-social.lib.networks.${network}`),
+      setting: `fof-share-social.networks.${network}`,
+      type: 'boolean',
+    })
   );
 });
